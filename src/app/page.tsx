c369388153a6dc2a0b240eb6ed51eb6fafa936d4
Chaand Bakshi <cbakshi@wisc.edu>
2024-06-20 14:13:57 -0500

    Added content in placeholders.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,35 +11,33 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
                 <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
                 <img src="/snake-1.png" className="w-full" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a{" "}
-                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
-                Phone Case
+                Buy, Sell and Trade Sneakers On The{" "}
+                <span className="bg-red-600 px-2 text-white">Blockchain</span>
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
-                Capture your favorite memories with your own,{" "}
-                <span className="font-semibold">one-of-one</span> phone case.
-                CaseCobra allows you to protect your memories, not just your
-                phone case.
+                WebSn3akers is your go to online marketplace for sneakers.
+                Collect unique
+                <span className="font-semibold"> NFTs</span> for each sneaker
+                listed and traded on WebSn3akers.
               </p>
 
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />
-                    High-quality, durable material
+                    <Check className="h-5 w-5 shrink-0 text-red-600" />
+                    Guaranteed security on the Etherium Blockchain.
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
-                    print guarantee
+                    <Check className="h-5 w-5 shrink-0 text-red-600" />
+                    Each NFT is unique to your sneaker.
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />
-                    Modern iPhone models supported
+                    <Check className="h-5 w-5 shrink-0 text-red-600" />
+                    Trade using the convenience of cryptocurrencies.
                   </li>
                 </div>
               </ul>
@@ -75,11 +73,11 @@ export default function Home() {
 
                 <div className="flex flex-col justify-between items-center sm:items-start">
                   <div className="flex gap-0.5">
-                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
-                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-red-600 fill-red-600" />
+                    <Star className="h-4 w-4 text-red-600 fill-red-600" />
+                    <Star className="h-4 w-4 text-red-600 fill-red-600" />
+                    <Star className="h-4 w-4 text-red-600 fill-red-600" />
+                    <Star className="h-4 w-4 text-red-600 fill-red-600" />
                   </div>
 
                   <p>
@@ -106,7 +104,6 @@ export default function Home() {
         </MaxWidthWrapper>
       </section>
 
-      {/* value proposition section */}
       <section className="bg-slate-100 grainy-dark py-24">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
@@ -119,11 +116,11 @@ export default function Home() {
           <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -145,7 +142,7 @@ export default function Home() {
                 <div className="flex flex-col">
                   <p className="font-semibold">Jonathan</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <Check className="h-4 w-4 stroke-[3px] text-red-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -155,11 +152,11 @@ export default function Home() {
             {/* second user review */}
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
-                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
+                <Star className="h-5 w-5 text-red-600 fill-red-600" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -182,7 +179,7 @@ export default function Home() {
                 <div className="flex flex-col">
                   <p className="font-semibold">Josh</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
+                    <Check className="h-4 w-4 stroke-[3px] text-red-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -200,7 +197,7 @@ export default function Home() {
             <div className="mx-auto max-w-2xl sm:text-center">
               <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
                 Upload your photo and get{" "}
-                <span className="relative px-2 bg-green-600 text-white">
+                <span className="relative px-2 bg-red-600 text-white">
                   your own case
                 </span>{" "}
                 now
@@ -228,19 +225,19 @@ export default function Home() {
 
           <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
             <li className="w-fit">
-              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-red-600 inline mr-1.5" />
               High-quality silicone material
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-red-600 inline mr-1.5" />
               Scratch- and fingerprint resistant coating
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              <Check className="h-5 w-5 text-red-600 inline mr-1.5" />
               Wireless charging compatible
             </li>
             <li className="w-fit">
-              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />5 year
+              <Check className="h-5 w-5 text-red-600 inline mr-1.5" />5 year
               print warranty
             </li>
 
